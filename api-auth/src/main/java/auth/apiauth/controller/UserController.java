6f468c8cb668852e75ad012d450363dc9a734a79
abner <abiner.anjos@gmail.com>
2020-04-13 15:29:10 -0300

    implement error handling and CRUD using query methods sql #4

--- api-auth/src/main/java/auth/apiauth/controller/UserController.java
+++ api-auth/src/main/java/auth/apiauth/controller/UserController.java
@@ -1,27 +1,16 @@
 package auth.apiauth.controller;
 
-import java.util.Collection;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 import auth.apiauth.model.User;
-import auth.apiauth.model.UserRepository;
 import auth.apiauth.services.UserServiceImpl;
-
-import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
 @RequestMapping(value = "/api_auth")
@@ -35,24 +24,40 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/list_users", method = RequestMethod.GET, produces = "application/json")
-	public List<User> list_users() {
-		return service.list();
+	public List<User> list_users(String name) {
+		try {
+			return service.list();
+		}  catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Users list not found can not connect", e);
+	   	}
 	}
 
 	@RequestMapping(value = "/find_by_name", method = RequestMethod.GET, produces = "application/json")
 	public User find_by_name(String user_name) {
-		return service.findOneByName(user_name);
+		try {
+			return service.findOneByName(user_name);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found", e);
+	   	}
 	}
 
 	@RequestMapping(value = "/find_by_code", method = RequestMethod.GET, produces = "application/json")
 	public User find_by_code(Long code) {
-		return service.findOneByCode(code);
+		try {
+			return service.findOneByCode(code);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found", e);
+	   	}
 	}
 
 	@RequestMapping(value = "/add_user", method = RequestMethod.GET, produces = "application/json")
 	public User add_user(Long code) {
 		if (service.add(new User())) {
-			return new User();
+			try {
+				return new User();
+			} catch (Exception e) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User can not be created", e);
+			}
 		} else {
 			return new User();
 		}
@@ -61,7 +66,11 @@ public class UserController {
 	@RequestMapping(value = "/remove_user", method = RequestMethod.GET, produces = "application/json")
 	public User remove_user(Long code) {
 		if (service.remove(new User())) {
-			return new User();
+			try {
+				return new User();
+			} catch (Exception e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User can not be deleted", e);
+			}
 		} else {
 			return new User();
 		}
@@ -70,7 +79,11 @@ public class UserController {
 	@RequestMapping(value = "/user_exists", method = RequestMethod.GET, produces = "application/json")
 	public User user_exists(Long code) {
 		if (service.exists(new User())) {
-			return new User();
+			try {
+				return new User();
+			} catch (Exception e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Connection refused", e);
+			}
 		} else {
 			return new User();
 		}
