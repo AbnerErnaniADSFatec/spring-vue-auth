4e8b2f8c96e39f16fc911121d2cd397f37e2bff0
abner <abiner.anjos@gmail.com>
2020-04-13 15:29:22 -0300

    implement error handling and CRUD using query methods sql #4

--- api-auth/src/main/java/auth/apiauth/model/UserRepository.java
+++ api-auth/src/main/java/auth/apiauth/model/UserRepository.java
@@ -1,7 +1,22 @@
 package auth.apiauth.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import auth.apiauth.model.User;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("select u from User u where u.nome like :nome%")
+    public List<User> findOneByName(@Param("nome") String nome);
+
+    @Query("select u from User u where u.codigo = :codigo")
+    public List<User> findOneByCode(@Param("codigo") Long codigo);
+
+}
