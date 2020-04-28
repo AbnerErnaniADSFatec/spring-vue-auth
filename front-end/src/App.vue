381863b34d4243c3dc6560e75eb64f5fd0bfe190
abner <abiner.anjos@gmail.com>
2020-04-28 14:22:48 -0300

    implementing css style eith routes #7

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id = "app">
-    <div id = "nav">
-      <router-link to = "/">Home</router-link> |
+  <div class = "root">
+    <h1 class = "is-center">Controle de Usuários</h1>
+    <nav class = "tabs is-center" id = "menu">
+      <router-link to = "/">Home</router-link>
       <router-link to = "/search">Search User</router-link>
+    </nav>
+    <div class = "app">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<style scoped>
+  @import './assets/styles/style.css';
+</style>
 
 <script>
   export default {
